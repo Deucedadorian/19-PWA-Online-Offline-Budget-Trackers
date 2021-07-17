f40832ceab8456673ea222b6bbffd52fa4142d6b
Dylan Cole <logboarddjc@gmail.com>
2021-07-17 09:52:34 -0400

    added babel core, changed bundle filename in webpack, added cached files in service worker

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -5,7 +5,7 @@ const FILES_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.webmanifest',
-  "/dist/bundle.js",
+  "/dist/main.bundle.js",
   '/style.css',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
